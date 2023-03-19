5f525f2a3e62ccdd9960ff290235eb5f67b0e224
Kuba429 <jakubzawadzki429@gmail.com>
2023-03-19 20:48:24 +0100

    window and grid

--- src/main.c
+++ src/main.c
@@ -1,6 +1,29 @@
+#include "raylib.h"
 #include <stdio.h>
 
+int windowHeight = 600;
+int windowWidth = 600;
+
+void DrawFrame();
+
 int main() {
-  printf("aaa");
+  InitWindow(windowWidth, windowHeight, "TicTacToe 7x7");
+  while (!WindowShouldClose()) {
+    DrawFrame();
+  }
   return 0;
+}
+
+void DrawFrame() {
+  BeginDrawing();
+
+  ClearBackground(WHITE);
+  for (int i = 1; i < 7; i++) {
+    //                                  make sure lines are included
+    int step = (windowHeight / 7) * i + (i - 1) * 2;
+    DrawLineEx((Vector2){0, step}, (Vector2){windowWidth, step}, 2.0, BLACK);
+    DrawLineEx((Vector2){step, 0}, (Vector2){step, windowHeight}, 2.0, BLACK);
+  }
+
+  EndDrawing();
 }
