8b27f9d66f8614b12754f72fe03bd551730f661f
Kuba429 <jakubzawadzki429@gmail.com>
2023-03-20 15:49:03 +0100

    get cell position

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@ int windowWidth = 600;
 
 void DrawFrame();
 void DrawMousePosition();
+void GetCellPosition(Vector2 mousePosition);
 
 int main() {
   InitWindow(windowWidth, windowHeight, "TicTacToe 7x7");
@@ -19,20 +20,29 @@ void DrawFrame() {
   BeginDrawing();
 
   ClearBackground(WHITE);
-  for (int i = 1; i < 7; i++) {
-    //                                  👇 make sure lines are included
-    int step = (windowHeight / 7) * i + (i - 1) * 2;
-    DrawLineEx((Vector2){0, step}, (Vector2){windowWidth, step}, 2.0, BLACK);
-    DrawLineEx((Vector2){step, 0}, (Vector2){step, windowHeight}, 2.0, BLACK);
-    DrawMousePosition();
-  }
+  //  for (int i = 1; i < 7; i++) {
+  //    //                                  👇 make sure lines are included
+  //    int step = (windowHeight / 7) * i + (i - 1) * 2;
+  //    DrawLineEx((Vector2){0, step}, (Vector2){windowWidth, step}, 2.0,
+  //    BLACK); DrawLineEx((Vector2){step, 0}, (Vector2){step,
+  //    windowHeight}, 2.0, BLACK);
+  //  }
+  DrawMousePosition();
+  GetCellPosition(GetMousePosition());
 
   EndDrawing();
 }
 
+void GetCellPosition(Vector2 mousePosition) {
+  char str[10];
+  int x = mousePosition.x / ((float)windowWidth / 7);
+  sprintf(str, "%d", (int)x);
+  DrawText(str, 1, 20, 20, BLACK);
+}
+
 void DrawMousePosition() {
   Vector2 mousePosition = GetMousePosition();
-  char positionStr[9];
+  char positionStr[11];
   sprintf(positionStr, "%d,%d", (int)mousePosition.x, (int)mousePosition.y);
   DrawText(positionStr, 1, 1, 20, BLACK);
 }
