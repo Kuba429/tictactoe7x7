c91bbe8567ed0bb682c8233ccb0a7ecf6ae7cdc6
Kuba429 <jakubzawadzki429@gmail.com>
2023-04-01 20:02:09 +0200

    optimized Get*WinningStreak functions

--- src/result.c
+++ src/result.c
@@ -41,26 +41,19 @@ int GetHorizontalWinner(int board[7][7], int x, int y) {
 }
 
 struct ListNode *GetHorizontalWinningStreak(int board[7][7], int x, int y) {
-  int original = board[y][x];
-  int largestX = x;
-  int smallestX = x;
-  // go right untill first occurence of different value
-  while (largestX + 1 < 7 && board[y][largestX + 1] == original) {
-    largestX++;
-  }
-  // then go left
-  while (smallestX - 1 >= 0 && board[y][smallestX - 1] == original) {
-    smallestX--;
-  }
-  // return null if not enough cells with the same value were next to each other
-  if (largestX <= smallestX || largestX - smallestX + 1 < 4) {
+  if (GetHorizontalWinner(board, x, y) == -1)
     return NULL;
-  }
+  int original = board[y][x];
+  int newX = x;
   struct ListNode *head = Push(NULL, x, y);
-  // avoid looping to the end of the list every time while still returning the
-  // head
   struct ListNode *lastNode = head;
-  for (int newX = smallestX; newX <= largestX; newX++) {
+  while (newX + 1 < 7 && board[y][newX + 1] == original) {
+    newX++;
+    lastNode = Push(lastNode, newX, y);
+  }
+  newX = x;
+  while (newX - 1 >= 0 && board[y][newX - 1] == original) {
+    newX--;
     lastNode = Push(lastNode, newX, y);
   }
   return head;
@@ -83,21 +76,19 @@ int GetVerticalWinner(int board[7][7], int x, int y) {
 }
 
 struct ListNode *GetVerticalWinningStreak(int board[7][7], int x, int y) {
-  int original = board[y][x];
-  int largestY = y;
-  int smallestY = y;
-  while (largestY + 1 < 7 && board[largestY + 1][x] == original) {
-    largestY++;
-  }
-  while (smallestY - 1 >= 0 && board[smallestY - 1][x] == original) {
-    smallestY--;
-  }
-  if (largestY <= smallestY || largestY - smallestY + 1 < 4) {
+  if (GetVerticalWinner(board, x, y) == -1)
     return NULL;
-  }
+  int original = board[y][x];
+  int newY = y;
   struct ListNode *head = Push(NULL, x, y);
   struct ListNode *lastNode = head;
-  for (int newY = smallestY; newY <= largestY; newY++) {
+  while (newY + 1 < 7 && board[newY + 1][x] == original) {
+    newY++;
+    lastNode = Push(lastNode, x, newY);
+  }
+  newY = y;
+  while (newY - 1 >= 0 && board[newY - 1][x] == original) {
+    newY--;
     lastNode = Push(lastNode, x, newY);
   }
   return head;
@@ -130,35 +121,28 @@ int GetDiagonalTopLeftWinner(int board[7][7], int x, int y) {
 
 struct ListNode *GetDiagonalTopLeftWinningStreak(int board[7][7], int x,
                                                  int y) {
-  int original = board[y][x];
-  int largestY = y;
-  int largestX = x;
-  int smallestY = y;
-  int smallestX = x;
-
-  // Check diagonal from top-left to bottom-right
-  while (largestY + 1 < 7 && largestX + 1 < 7 &&
-         board[largestY + 1][largestX + 1] == original) {
-    largestY++;
-    largestX++;
-  }
-  while (smallestY - 1 >= 0 && smallestX - 1 >= 0 &&
-         board[smallestY - 1][smallestX - 1] == original) {
-    smallestY--;
-    smallestX--;
-  }
-  if (largestY - smallestY + 1 < 4) {
-    // Not a win
+  if (GetDiagonalTopLeftWinner(board, x, y) == -1)
     return NULL;
-  }
-
+  int original = board[y][x];
+  int newY = y;
+  int newX = x;
   struct ListNode *head = Push(NULL, x, y);
   struct ListNode *lastNode = head;
-  for (int newY = smallestY, newX = smallestX;
-       newY <= largestY && newX <= largestX; newY++, newX++) {
+
+  while (newY + 1 < 7 && newX + 1 < 7 &&
+         board[newY + 1][newX + 1] == original) {
+    newY++;
+    newX++;
     lastNode = Push(lastNode, newX, newY);
   }
-
+  newY = y;
+  newX = x;
+  while (newY - 1 >= 0 && newX - 1 >= 0 &&
+         board[newY - 1][newX - 1] == original) {
+    newY--;
+    newX--;
+    lastNode = Push(lastNode, newX, newY);
+  }
   return head;
 }
 
@@ -186,34 +170,27 @@ int GetDiagonalTopRightWinner(int board[7][7], int x, int y) {
 }
 struct ListNode *GetDiagonalTopRightWinningStreak(int board[7][7], int x,
                                                   int y) {
-  int original = board[y][x];
-  int largestY = y;
-  int largestX = x;
-  int smallestY = y;
-  int smallestX = x;
-
-  // Check diagonal from top-right to bottom-left
-  while (largestY + 1 < 7 && smallestX - 1 >= 0 &&
-         board[largestY + 1][smallestX - 1] == original) {
-    largestY++;
-    smallestX--;
-  }
-  while (smallestY - 1 >= 0 && largestX + 1 < 7 &&
-         board[smallestY - 1][largestX + 1] == original) {
-    smallestY--;
-    largestX++;
-  }
-  if (largestY - smallestY + 1 < 4) {
-    // Not a win
+  if (GetDiagonalTopRightWinner(board, x, y) == -1)
     return NULL;
-  }
-
+  int original = board[y][x];
+  int newY = y;
+  int newX = x;
   struct ListNode *head = Push(NULL, x, y);
   struct ListNode *lastNode = head;
-  for (int newY = smallestY, newX = largestX;
-       newY <= largestY && newX >= smallestX; newY++, newX--) {
+
+  while (newY + 1 < 7 && newX - 1 >= 0 &&
+         board[newY + 1][newX - 1] == original) {
+    newY++;
+    newX--;
     lastNode = Push(lastNode, newX, newY);
   }
-
+  newY = y;
+  newX = x;
+  while (newY - 1 >= 0 && newX + 1 < 7 &&
+         board[newY - 1][newX + 1] == original) {
+    newY--;
+    newX++;
+    lastNode = Push(lastNode, newX, newY);
+  }
   return head;
 }
